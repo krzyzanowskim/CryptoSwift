978a7ac84ee34df57ae1995dcdb7ef2ce5f2351d
Pyry Jahkola <pyry.jahkola@iki.fi>
2015-06-21 21:00:19 +0300

    Add CryptoSwiftDemo, to be used as iOS test runner

---
+++ CryptoSwiftDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CryptoSwiftDemo
+//
+//  Copyright (c) 2015 Marcin Krzyzanowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

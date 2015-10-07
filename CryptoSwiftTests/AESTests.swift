1a03ddb9a98810abf9713047f9fc479c133f1638
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2015-10-07 15:15:24 +0200

    Use calculated sBox

--- CryptoSwiftTests/AESTests.swift
+++ CryptoSwiftTests/AESTests.swift
@@ -122,8 +122,9 @@ final class AESTests: XCTestCase {
         let key:[UInt8] = [0x2b,0x7e,0x15,0x16,0x28,0xae,0xd2,0xa6,0xab,0xf7,0x15,0x88,0x09,0xcf,0x4f,0x3c];
         let iv:[UInt8] = [0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F]
         let message = [UInt8](count: 1024 * 1024, repeatedValue: 7)
+        let aes = AES(key: key, iv: iv, blockMode: .CBC)
         measureMetrics([XCTPerformanceMetric_WallClockTime], automaticallyStartMeasuring: true, forBlock: { () -> Void in
-            try! AES(key: key, iv: iv, blockMode: .CBC)?.encrypt(message, padding: PKCS7())
+            try! aes?.encrypt(message, padding: PKCS7())
         })
     }
 
@@ -131,8 +132,9 @@ final class AESTests: XCTestCase {
         let key:[UInt8] = [0x2b,0x7e,0x15,0x16,0x28,0xae,0xd2,0xa6,0xab,0xf7,0x15,0x88,0x09,0xcf,0x4f,0x3c];
         let iv:[UInt8] = [0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F]
         let message = [UInt8](count: 1024 * 1024, repeatedValue: 7)
+        let aes = AES(key: key, iv: iv, blockMode: .CBC)
         measureMetrics([XCTPerformanceMetric_WallClockTime], automaticallyStartMeasuring: true, forBlock: { () -> Void in
-            try! AES(key: key, iv: iv, blockMode: .CBC)?.decrypt(message, padding: PKCS7())
+            try! aes?.decrypt(message, padding: PKCS7())
         })
     }
 

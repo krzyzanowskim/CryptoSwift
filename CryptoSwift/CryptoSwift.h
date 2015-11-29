b33c9fd73a2c973b422155953f7530d73afd6ab0
Sam Soffes <sam@soff.es>
2015-11-29 13:40:38 -0800

    Add umbrella header

---
+++ CryptoSwift/CryptoSwift.h
@@ -0,0 +1,15 @@
+//
+//  CryptoSwift.h
+//  CryptoSwift
+//
+//  Created by Sam Soffes on 11/29/15.
+//  Copyright © 2015 Marcin Krzyzanowski. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for CryptoSwift.
+FOUNDATION_EXPORT double CryptoSwiftVersionNumber;
+
+//! Project version string for CryptoSwift.
+FOUNDATION_EXPORT const unsigned char CryptoSwiftVersionString[];

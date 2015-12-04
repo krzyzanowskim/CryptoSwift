a6161751b20501de2f9584f0f2be4ffe8e37afc1
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2015-12-04 01:35:52 +0100

    cleanup Package.swift

--- SwiftPackageManager/Package.swift
+++ SwiftPackageManager/Package.swift
@@ -2,7 +2,4 @@ import PackageDescription
 
 let package = Package(
     name: "CryptoSwift"
-    // dependencies: [
-    //     .Package(url: "file:///home/parallels/Desktop/Parallels Shared Folders/Home/Devel/CryptoSwift", versions: "0.0.1")
-    // ]
 )

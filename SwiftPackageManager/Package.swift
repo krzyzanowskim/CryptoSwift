0a8e174d4278b314de1195a8e4ad9079f881691f
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2015-12-04 01:11:44 +0100

    Move Package.swift to dedicated "SwiftPAckageManager" folder with valid folder layout

---
+++ SwiftPackageManager/Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "CryptoSwift"
+    // dependencies: [
+    //     .Package(url: "file:///home/parallels/Desktop/Parallels Shared Folders/Home/Devel/CryptoSwift", versions: "0.0.1")
+    // ]
+)

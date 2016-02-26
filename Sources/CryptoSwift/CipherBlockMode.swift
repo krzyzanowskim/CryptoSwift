7ae4159b52df32517bf8a0e1c50cb1204c7a8187
Matteo Crippa <matteocrippa@users.noreply.github.com>
2016-02-26 17:46:43 +0100

    fixed swift 3.0 deprecations

--- Sources/CryptoSwift/CipherBlockMode.swift
+++ Sources/CryptoSwift/CipherBlockMode.swift
@@ -212,7 +212,7 @@ private struct CTRMode: BlockMode {
         var out:[UInt8] = [UInt8]()
         out.reserveCapacity(blocks.count * blocks[blocks.startIndex].count)
         for plaintext in blocks {
-            let nonce = buildNonce(iv, counter: UInt64(counter++))
+            let nonce = buildNonce(iv, counter: UInt64(counter + 1))
             if let encrypted = cipherOperation(block: nonce) {
                 out.appendContentsOf(xor(plaintext, encrypted))
             }

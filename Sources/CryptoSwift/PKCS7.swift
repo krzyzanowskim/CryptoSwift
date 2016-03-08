849f85ff7bc6a1f3e117980665e4cf1409960c65
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2016-03-08 23:08:34 +0100

    For invalid input return input instead of crash

--- Sources/CryptoSwift/PKCS7.swift
+++ Sources/CryptoSwift/PKCS7.swift
@@ -37,6 +37,11 @@ public struct PKCS7: Padding {
     }
 
     public func remove(bytes: [UInt8], blockSize:Int?) -> [UInt8] {
+        assert(bytes.count > 0, "Need bytes to remove padding")
+        guard bytes.count > 0 else {
+            return bytes
+        }
+
         let lastByte = bytes.last!
         let padding = Int(lastByte) // last byte
         let finalLength = bytes.count - padding

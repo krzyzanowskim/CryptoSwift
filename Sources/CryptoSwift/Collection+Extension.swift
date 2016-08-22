0f0c63fab0a35f65e08529d172e1200c954c9002
Michael Ledin <m.ledin@appheads.ru>
2016-08-23 00:06:04 +0300

    Revert "Bugfix and optimization."
    This reverts commit c6254e4a25b37d368eaaa0e76dfa4cd0a3a8c9c9.

--- Sources/CryptoSwift/Collection+Extension.swift
+++ Sources/CryptoSwift/Collection+Extension.swift
@@ -8,17 +8,45 @@
 
 extension Collection where Self.Iterator.Element == UInt8, Self.Index == Int {
     func toUInt32Array() -> Array<UInt32> {
-        return stride(from: self.startIndex, to: self.endIndex, by: MemoryLayout<UInt32>.size).map { i in
-            return UnsafePointer(Array(self[Range(i..<i.advanced(by:MemoryLayout<UInt32>.size))]))
-                .withMemoryRebound(to: UInt32.self, capacity: 1) { $0.pointee }
+        var result = Array<UInt32>()
+        result.reserveCapacity(16)
+        for idx in stride(from: self.startIndex, to: self.endIndex, by: MemoryLayout<UInt32>.size) {
+            var val: UInt32 = 0
+            val |= self.count > 3 ? UInt32(self[idx.advanced(by: 3)]) << 24 : 0
+            val |= self.count > 2 ? UInt32(self[idx.advanced(by: 2)]) << 16 : 0
+            val |= self.count > 1 ? UInt32(self[idx.advanced(by: 1)]) << 8  : 0
+            val |= self.count > 0 ? UInt32(self[idx]) : 0
+            result.append(val)
         }
+
+        for _ in result.count..<MemoryLayout<UInt32>.size {
+            result.append(0)
+        }
+
+        return result
     }
 
     func toUInt64Array() -> Array<UInt64> {
-        return stride(from: self.startIndex, to: self.endIndex, by: MemoryLayout<UInt64>.size).map { i in
-            return UnsafePointer(Array(self[Range(i..<i.advanced(by:MemoryLayout<UInt64>.size))]))
-                .withMemoryRebound(to: UInt64.self, capacity: 1) { $0.pointee }
+        var result = Array<UInt64>()
+        result.reserveCapacity(32)
+        for idx in stride(from: self.startIndex, to: self.endIndex, by: MemoryLayout<UInt64>.size) {
+            var val:UInt64 = 0
+            val |= self.count > 7 ? UInt64(self[idx.advanced(by: 7)]) << 56 : 0
+            val |= self.count > 6 ? UInt64(self[idx.advanced(by: 6)]) << 48 : 0
+            val |= self.count > 5 ? UInt64(self[idx.advanced(by: 5)]) << 40 : 0
+            val |= self.count > 4 ? UInt64(self[idx.advanced(by: 4)]) << 32 : 0
+            val |= self.count > 3 ? UInt64(self[idx.advanced(by: 3)]) << 24 : 0
+            val |= self.count > 2 ? UInt64(self[idx.advanced(by: 2)]) << 16 : 0
+            val |= self.count > 1 ? UInt64(self[idx.advanced(by: 1)]) << 8 : 0
+            val |= self.count > 0 ? UInt64(self[idx.advanced(by: 0)]) << 0 : 0
+            result.append(val)
         }
+
+        for _ in result.count..<MemoryLayout<UInt64>.size {
+            result.append(0)
+        }
+
+        return result
     }
 
     /// Initialize integer from array of bytes. Caution: may be slow!

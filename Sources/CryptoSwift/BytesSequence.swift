3351a605c5379bea01f3dd5542d993fb1ce848e7
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2016-10-14 04:19:00 +0200

    Use specialized version of BytesSequence due to performance resons.

--- Sources/CryptoSwift/BytesSequence.swift
+++ Sources/CryptoSwift/BytesSequence.swift
@@ -6,11 +6,31 @@
 //  Copyright © 2015 Marcin Krzyzanowski. All rights reserved.
 //
 
-struct BytesSequence<D: RandomAccessCollection>: Sequence where D.Iterator.Element == UInt8, D.IndexDistance == Int, D.SubSequence.IndexDistance == Int, D.Index == Int {
-    let chunkSize: D.IndexDistance
-    let data: D
+/// Generic version of BytesSequence is slower, therefore specialized version is in use
+///
+//struct BytesSequence<D: RandomAccessCollection>: Sequence where D.Iterator.Element == UInt8, D.IndexDistance == Int, D.SubSequence.IndexDistance == Int, D.Index == Int {
+//    let chunkSize: D.IndexDistance
+//    let data: D
+//
+//    func makeIterator() -> AnyIterator<D.SubSequence> {
+//        var offset = data.startIndex
+//        return AnyIterator {
+//            let end = Swift.min(self.chunkSize, self.data.count - offset)
+//            let result = self.data[offset..<offset + end]
+//            offset = offset.advanced(by: result.count)
+//            if !result.isEmpty {
+//                return result
+//            }
+//            return nil
+//        }
+//    }
+//}
 
-    func makeIterator() -> AnyIterator<D.SubSequence> {
+struct BytesSequence: Sequence {
+    let chunkSize: Array<UInt8>.IndexDistance
+    let data: Array<UInt8>
+
+    func makeIterator() -> AnyIterator<ArraySlice<UInt8>> {
         var offset = data.startIndex
         return AnyIterator {
             let end = Swift.min(self.chunkSize, self.data.count - offset)

29698721591ccf049cc7e615c7b7a90d3b0a8ddc
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2017-04-30 14:38:32 +0200

    Workaround CI build time issues. Fixes #418.

--- scripts/CI.sh
+++ scripts/CI.sh
@@ -22,7 +22,8 @@ if [ -d Packages ]; then
 	fi
 fi
 
-swift build
+# https://github.com/krzyzanowskim/CryptoSwift/issues/418
+swift build -Xswiftc -Xfrontend -Xswiftc -solver-memory-threshold -Xswiftc -Xfrontend -Xswiftc 999999999
 sbexit=$?
 
 echo "Swift build exited with code $sbexit"

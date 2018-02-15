b869d2efcf671ab14103857156e398575203d1aa
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2018-02-15 10:37:05 +0100

    Update version to 0.8.2

--- Sources/CryptoSwift/BlockMode/BlockModeOptions.swift
+++ Sources/CryptoSwift/BlockMode/BlockModeOptions.swift
@@ -16,7 +16,7 @@
 struct BlockModeOptions: OptionSet {
     let rawValue: Int
 
-    static let none = BlockModeOptions(rawValue: 0)
-    static let initializationVectorRequired = BlockModeOptions(rawValue: 1)
-    static let paddingRequired = BlockModeOptions(rawValue: 2)
+    static let none = BlockModeOptions(rawValue: 1 << 0)
+    static let initializationVectorRequired = BlockModeOptions(rawValue: 1 << 1)
+    static let paddingRequired = BlockModeOptions(rawValue: 1 << 2)
 }

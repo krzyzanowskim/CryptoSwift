c986037d575b226bfde93ad63190d7f98789f39b
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2018-10-16 11:24:32 +0200

    Add StreamDecryptor

--- Sources/CryptoSwift/AES.Cryptors.swift
+++ Sources/CryptoSwift/AES.Cryptors.swift
@@ -27,6 +27,9 @@ extension AES: Cryptors {
     public func makeDecryptor() throws -> Cryptor & Updatable {
         let cipherOperation: CipherOperationOnBlock = blockMode.options.contains(.useEncryptToDecrypt) == true ? encrypt : decrypt
         let worker = try blockMode.worker(blockSize: AES.blockSize, cipherOperation: cipherOperation)
+        if worker is StreamModeWorker {
+            return try StreamDecryptor(blockSize: AES.blockSize, padding: padding, worker)
+        }
         return try BlockDecryptor(blockSize: AES.blockSize, padding: padding, worker)
     }
 }

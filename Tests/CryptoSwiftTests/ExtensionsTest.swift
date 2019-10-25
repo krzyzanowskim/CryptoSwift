e699ada94a9ef7a96f77ba8fa17ad0a2c1c78f21
Valeriy Van <github@w7software.com>
2019-10-25 21:40:38 +0200

    Adds performance test for toHexString() from [UInt8] extension

--- Tests/CryptoSwiftTests/ExtensionsTest.swift
+++ Tests/CryptoSwiftTests/ExtensionsTest.swift
@@ -82,6 +82,19 @@ final class ExtensionsTest: XCTestCase {
     let hex = array.toHexString()
     XCTAssertEqual(str, hex)
   }
+
+  func testToHexStringPerformance() {
+    let len = 100000
+    let a = [UInt8](unsafeUninitializedCapacity: len) { buf, count in
+        for i in 0..<len {
+            buf[i] = UInt8.random(in: 0...UInt8.max)
+        }
+        count = len
+    }
+    self.measure {
+        _ = a.toHexString()
+    }
+  }
 }
 
 extension ExtensionsTest {

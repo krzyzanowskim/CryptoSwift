8ee88c7587be82a7c8ec0de4f882628a4f3768e5
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2020-10-30 13:56:43 +0100

    Xcode can't swiftSettings.unsafeFlags

--- Package.swift
+++ Package.swift
@@ -14,15 +14,7 @@ let package = Package(
     )
   ],
   targets: [
-    .target(
-      name: "CryptoSwift",
-            swiftSettings: [
-            // Enable better optimizations when building in Release configuration. Despite the use of
-            // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-            // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
-            .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
-        ]
-),
+    .target(name: "CryptoSwift"),
     .testTarget(name: "CryptoSwiftTests", dependencies: ["CryptoSwift"]),
     .testTarget(name: "TestsPerformance", dependencies: ["CryptoSwift"])
   ],

c60d6b061b52e7e0fffd833b81bc6b13052ddd18
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2020-10-30 13:49:21 +0100

    Enable -cross-module-optimization for SPM release builds

--- Package.swift
+++ Package.swift
@@ -14,7 +14,15 @@ let package = Package(
     )
   ],
   targets: [
-    .target(name: "CryptoSwift"),
+    .target(
+      name: "CryptoSwift",
+            swiftSettings: [
+            // Enable better optimizations when building in Release configuration. Despite the use of
+            // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+            // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
+            .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+        ]
+),
     .testTarget(name: "CryptoSwiftTests", dependencies: ["CryptoSwift"]),
     .testTarget(name: "TestsPerformance", dependencies: ["CryptoSwift"])
   ],

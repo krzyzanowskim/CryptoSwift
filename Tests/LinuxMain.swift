f6b012c0bf853a2d49c227bf4a72c43ab744c15e
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2024-04-11 20:38:05 +0200

    Remove reference to removed tests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,10 +1,8 @@
 import XCTest
 
 import CryptoSwiftTests
-import TestsPerformance
 
 var tests = [XCTestCaseEntry]()
 tests += CryptoSwiftTests.__allTests()
-tests += TestsPerformance.__allTests()
 
 XCTMain(tests)

b3e2f39c23aa1ab5aa586bc984d961e18cbd5b81
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2025-12-22 01:31:11 +0100

    Fix typos

--- Sources/CryptoSwift/Blowfish.swift
+++ Sources/CryptoSwift/Blowfish.swift
@@ -6,7 +6,7 @@
 //
 //  In no event will the authors be held liable for any damages arising from the use of this software.
 //
-//  Permission is granted to anyone to use this software for any purpose,including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+//  Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
 //
 //  - The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation is required.
 //  - Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
@@ -535,7 +535,7 @@ extension Blowfish: Cipher {
     out.reserveCapacity(bytes.count)
 
     for chunk in Array(bytes).batched(by: Blowfish.blockSize) {
-      out += self.decryptWorker.decrypt(block: chunk) // FIXME: copying here is innefective
+      out += self.decryptWorker.decrypt(block: chunk) // FIXME: copying here is ineffective
     }
 
     out = self.padding.remove(from: out, blockSize: Blowfish.blockSize)
